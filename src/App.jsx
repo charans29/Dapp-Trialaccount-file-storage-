fb097238a704ca5949bbadf41312e0c442125876
Aayush Jaiswal <aay7ush@gmail.com>
2024-01-23 20:44:36 +0530

    refactor: change layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { Contents } from "./components/Contents"
-import { Navbar } from "./components/Navbar"
-import { useState } from "react";
-
+import { useState } from "react"
+import DisplayFiles from "./components/DisplayFiles"
+import DragFiles from "./components/DragFiles"
 
 function App() {
-  const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([])
 
   return (
-    <div className="relative">
-      <Navbar />
-      <Contents files={files} setFiles={setFiles}/>
+    <div className="min-h-screen place-content-center grid grid-cols-1 md:grid-cols-2 gap-7 px-5 md:px-7 my-10 md:my-0">
+      <DragFiles files={files} setFiles={setFiles} />
+      <DisplayFiles files={files} setFiles={setFiles} />
     </div>
   )
 }
