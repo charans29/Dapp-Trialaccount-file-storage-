e7dd62d527276b00394050bd65a1c7ba5deac93d
Aayush Jaiswal <aay7ush@gmail.com>
2024-01-23 20:43:46 +0530

    refactor: cleanup

--- src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export function Navbar(){
-    return(<div className="w-full h-20">
-            <div className="bg-blue-200 h-20 fixed w-full z-10">
-                navbar...
-            </div>
-        </div>)
-}
